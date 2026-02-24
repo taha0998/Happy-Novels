344af3c191b4bad5ef159340460ad705ca47e055
taha0998 <taha2020tahataha@gmail.com>
2026-02-24 16:18:20 +0000

    to-action-state, sign-up

---
+++ components/form/utils/to-action-state.ts
@@ -0,0 +1,53 @@
+import { ZodError } from 'zod'
+
+export type ActionState = {
+    status?: "SUCCESS" | "ERROR";
+    message: string;
+    payload?: FormData;
+    fieldError: Record<string, string[] | undefined>;
+    timestamp: number;
+}
+
+export const EPMTY_ACTION_STATE: ActionState = {
+    message: "",
+    fieldError: {},
+    timestamp: Date.now()
+}
+
+export const fromErrorToActionState = (error: unknown, formData?: FormData): ActionState => {
+    if (error instanceof ZodError) {
+        return {
+            status: "ERROR",
+            message: '',
+            fieldError: error.flatten().fieldErrors,
+            payload: formData,
+            timestamp: Date.now()
+        }
+    } else if (error instanceof Error) {
+        return {
+            status: 'ERROR',
+            message: error.message,
+            fieldError: {},
+            payload: formData,
+            timestamp: Date.now()
+        }
+    } else {
+        return {
+            status: 'ERROR',
+            message: 'An unknown error occured',
+            fieldError: {},
+            payload: formData,
+            timestamp: Date.now()
+        }
+    }
+}
+
+export const toActionState = (status: ActionState['status'], message: string) => {
+    return {
+        status,
+        message,
+        fieldError: {},
+        timeStamp: Date.now()
+    }
+
+}
